50398d3b7a20f984b15698748df5ee3f3b03759b
moud <haibou177@gmail.com>
2023-10-19 14:17:46 +0000

    phase4 completed

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const { restoreUser, setTokenCookie, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
 });
+
 
 module.exports = router;
